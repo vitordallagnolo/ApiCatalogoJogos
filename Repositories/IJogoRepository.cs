d94406a218d5d3afb3e9b5087d82768023cec4c3
Vitor Dallagnolo <86325265+vitordallagnolo@users.noreply.github.com>
2021-11-14 15:26:50 -0300

    Upload feito de uma vez do projeto todo
    Projeto realizado durante o curso "Criando um catálogo de jogos usando boas práticas de arquitetura com .NET" da DIO.
    Instrutor Thiago Campos de Oliveira

---
+++ Repositories/IJogoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using ApiCatalogoJogos.Entities;
+
+namespace ApiCatalogoJogos.Repositories
+{
+    public interface IJogoRepository : IDisposable
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
+    }
+}
